c722811716b65210c719f38e9cd62b086305a18d
Fikret Ceylan <ofcyln@gmail.com>
2018-10-26 02:27:58 +0300

    FillSwitchComponent integration to fill jokes automatically

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,9 +4,10 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AlertModule } from './alert/alert.module';
 import { AppRoutingModule } from '../app-routing.module';
+import { FillSwitchComponent } from './header/fill-switch/fill-switch.component';
 
 @NgModule({
-    declarations: [HeaderComponent],
+    declarations: [HeaderComponent, FillSwitchComponent],
     imports: [CommonModule, AlertModule, AppRoutingModule],
     exports: [HeaderComponent, AlertModule, AppRoutingModule],
 })
---
+++ src/app/core/header/fill-switch/fill-switch.component.ts
@@ -0,0 +1,13 @@
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+
+@Component({
+    selector: 'app-fill-switch',
+    templateUrl: './fill-switch.component.html',
+    styleUrls: ['./fill-switch.component.scss'],
+    encapsulation: ViewEncapsulation.None,
+})
+export class FillSwitchComponent implements OnInit {
+    constructor() {}
+
+    ngOnInit() {}
+}
