f64bfab4662d4f7c2f0c108d8f5d4449258f2f5a
Fikret Ceylan <ofcyln@gmail.com>
2018-10-27 00:15:08 +0300

    FillerSwitchComponent moved under FavoritesComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,9 +11,16 @@ import { NotFoundComponent } from './not-found/not-found.component';
 import { StorageService } from './shared/service/storage.service';
 import { CoreModule } from './core/core.module';
 import { JokesService } from './shared/service/jokes.service';
+import { FillSwitchComponent } from './favorites/fill-switch/fill-switch.component';
 
 @NgModule({
-    declarations: [AppComponent, JokesComponent, FavoritesComponent, NotFoundComponent],
+    declarations: [
+        AppComponent,
+        JokesComponent,
+        FavoritesComponent,
+        FillSwitchComponent,
+        NotFoundComponent,
+    ],
     imports: [BrowserModule, HttpClientModule, AuthModule, CoreModule, AppRoutingModule],
     providers: [StorageService, JokesService],
     bootstrap: [AppComponent],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,10 +4,9 @@ import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
 import { AlertModule } from './alert/alert.module';
 import { AppRoutingModule } from '../app-routing.module';
-import { FillSwitchComponent } from './header/fill-switch/fill-switch.component';
 
 @NgModule({
-    declarations: [HeaderComponent, FillSwitchComponent],
+    declarations: [HeaderComponent],
     imports: [CommonModule, AlertModule, AppRoutingModule],
     exports: [HeaderComponent, AlertModule, AppRoutingModule],
 })
